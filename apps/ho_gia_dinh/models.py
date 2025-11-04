cd9063b5e39c2368462d4773d9dae84c10e069d6
doandangkhoa <doandangkhoa150505@gmail.com>
2025-11-04 17:08:51 +0700

    update models

--- apps/ho_gia_dinh/models.py
+++ apps/ho_gia_dinh/models.py
@@ -1,13 +1,13 @@
 from django.db import models
+from apps.nhan_khau.models import NhanKhau
 
 class HoGiaDinh(models.Model):
+    id_chu_ho = models.OneToOneField(NhanKhau, max_length=20, unique=True, on_delete=models.SET_NULL, null=True, blank=True, related_name='chu_ho_cua')
     so_ho_khau = models.CharField(max_length=20, unique=True)
     ho_ten_chu_ho = models.CharField(max_length=100)
     so_dien_thoai = models.CharField(max_length=50, null=True, blank=True)
-    so_nha = models.CharField(max_length=50)
-    duong_pho = models.CharField(max_length=100)
+    dia_chi = models.CharField(max_length=100)
     phuong_xa = models.CharField(max_length=100)
-    quan_huyen = models.CharField(max_length=100)
     ngay_tao = models.DateField(auto_now_add=True)
     ghi_chu = models.CharField(max_length=255, null=True, blank=True)
 
